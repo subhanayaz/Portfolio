e24061e7b272b23e8611add4c6a61d678f1060fb
Mohammed Subhan Ayaz Zama <66430769+subhanayaz@users.noreply.github.com>
2021-12-06 14:26:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ const form = document.forms['google-sheet']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+        .then(response => alert("Thanks for Contacting..! Will Contact You Soon..."))
         .catch(error => console.error('Error!', error.message))
 })
