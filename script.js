70899085a5dc3d11bb4aff982b1e0ac8a2dbb0b5
Mohammed Subhan Ayaz Zama <66430769+subhanayaz@users.noreply.github.com>
2021-12-06 14:23:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+//  Change Navbar Color on scroll 
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.backgroundColor = "white";
+    }
+    else {
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+
+    }
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwx9W0jdO665WBb4hFjOgcunBl-M58axnd0_UuCj1evUDnJ8_YZSpOM3LfJL9BuFUhEiw/exec'
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+        .catch(error => console.error('Error!', error.message))
+})
